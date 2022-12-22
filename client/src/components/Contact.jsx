211a488ab2839c246b3be3d486bfe585916a3b1f
Jonathan <navakino@gmail.com>
2022-12-22 15:55:00 -0600

    Change cursor to pointer when hovering on contact icons.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -6,13 +6,13 @@ const Contact = () => {
       <div className="text-center text-white text-5xl p-4 m-2">Contact</div>
       <div className="text-center text-white text-2xl p-4 m-2">Contact me on LinkedIn or send me an email. You should also check out my GitHub.</div>
       <div className="flex-row text-center space-x-12 pb-12 mb-12">
-        <i className="fa-brands fa-linkedin fa-3x text-white hover:text-rose-800" onClick={() => {
+        <i className="fa-brands fa-linkedin fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('https://www.linkedin.com/in/jonathan-navarrete');
         }}></i>
-        <i className="fa-regular fa-envelope fa-3x text-white hover:text-rose-800" onClick={() => {
+        <i className="fa-regular fa-envelope fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('mailto:jonathan3navarrete@gmail.com');
         }}></i>
-        <i className="fa-brands fa-github fa-3x text-white hover:text-rose-800" onClick={() => {
+        <i className="fa-brands fa-github fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('https://github.com/Ragnaric');
         }}></i>
       </div>
