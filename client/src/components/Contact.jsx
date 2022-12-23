e75bbffdc277f437505d96160e5d5da88d9d338a
Jonathan <navakino@gmail.com>
2022-12-23 00:22:23 -0600

    Restyle the css to make the app more responsive.

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -9,16 +9,16 @@ const Contact = () => {
 
   return (
     <div className={`${inView ? 'transition-opacity' : 'opacity-0'} w-7/12 m-auto pt-24 mt-24 justify-center duration-[2000ms]`} ref={ref}>
-      <div className="text-center text-rose-800 text-5xl p-4 m-2">Contact</div>
-      <div className="text-center text-white text-2xl p-4 m-2">Contact me on LinkedIn or send me an email. You should also check out my GitHub.</div>
+      <div className="text-center text-rose-800 text-[3vw] p-4 m-2">Contact</div>
+      <div className="text-center text-white text-[2vw] p-4 m-2">Contact me on LinkedIn or send me an email. You should also check out my GitHub.</div>
       <div className="flex-row text-center space-x-12 pb-12 mb-12">
-        <i className="fa-brands fa-linkedin fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
+        <i className="fa-brands fa-linkedin text-[3vw] text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('https://www.linkedin.com/in/jonathan-navarrete');
         }}></i>
-        <i className="fa-regular fa-envelope fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
+        <i className="fa-regular fa-envelope text-[3vw] text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('mailto:jonathan3navarrete@gmail.com');
         }}></i>
-        <i className="fa-brands fa-github fa-3x text-white hover:text-rose-800 cursor-pointer" onClick={() => {
+        <i className="fa-brands fa-github text-[3vw] text-white hover:text-rose-800 cursor-pointer" onClick={() => {
           window.open('https://github.com/Ragnaric');
         }}></i>
       </div>
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
 
   return (
     <div className={`${inView ? 'transition-opacity' : 'opacity-0'} pt-24 mt-24 flex-col space-y-8 justify-center duration-[2000ms]`} ref={ref}>
-      <h1 className="w-1/2 m-auto text-rose-800 text-center text-5xl p-2 my-2">Projects</h1>
+      <h1 className="w-1/2 m-auto text-rose-800 text-center text-[3vw] p-2 my-2">Projects</h1>
       <ProjectCard />
     </div>
   );
