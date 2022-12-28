41ceada75f714ce429c28d099f741fbdd0abbe96
Jonathan <navakino@gmail.com>
2022-12-28 13:33:51 -0600

    Create hamburger menu icon on right corner.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
 const Navigation = ({about, projects, contact}) => {
+
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <nav className="flex w-full fixed z-10 py-2.5 border-b bg-rose-800 leading-10 h-20">
       <div className="container flex flex-row space-x-12 m-auto w-11/12 relative text-center items-center">
@@ -22,9 +26,13 @@ const Navigation = ({about, projects, contact}) => {
         <div className="hidden lg:inline-flex lg:text-white lg:text-3xl lg:absolute lg:right-0 lg:bottom-0 lg:hover:text-black lg:hover:cursor-pointer" onClick={() => {
           window.open('./assets/Jonathan Navarrete Résumé.docx.pdf');
         }}>Resume</div>
-        <div className="inline-flex text-white text-3xl absolute right-0 hover:text-black hover:cursor-pointer lg:hidden" onClick={() => {
-          window.open('./assets/Jonathan Navarrete Résumé.docx.pdf');
-        }}>Menu</div>
+        <div className="flex w-8 h-6 inline-flex flex-col space-y-2 text-white text-3xl absolute right-0 lg:hidden" onClick={() => {
+          setIsActive(!isActive);
+        }}>
+          <span className="w-full h-1 bg-white"></span>
+          <span className={`${!isActive ? 'w-full h-1 bg-white' : ''}`}></span>
+          <span className="w-full h-1 bg-white"></span>
+        </div>
       </div>
     </nav>
 
